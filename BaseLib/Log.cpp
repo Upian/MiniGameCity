ac2e585d5fc88a2e4174a9598ea6e436877047d8
원형 이 <whlee@joycity.com>
2020-01-15 17:22:32 +0900

    Singleton Log 추가

---
+++ BaseLib/Log.cpp
@@ -0,0 +1,42 @@
+#include "Log.h"
+#include <cstdarg>
+namespace Util {
+	LogManager::LogManager() {
+		m_writeFile = new std::ofstream();
+	}
+
+	LogManager::LogManager(const char* directory) :m_directory(directory) {
+		m_writeFile = new std::ofstream();
+	}
+
+	LogManager::~LogManager() {
+		if (nullptr != m_writeFile)
+			delete m_writeFile;
+		m_writeFile = nullptr;
+	}
+
+
+	void Util::Logging(std::string fileName, const char* msg, ...) {
+		int len = 0;
+		char msgBuf[1024] = { 0, };
+
+		va_list ap;
+		va_start(ap, msg);
+
+		len = vsprintf_s(msgBuf, 1024, msg, ap);
+		msgBuf[len] = '\0';
+
+		va_end(ap);
+
+		std::cout << "Log message: " << msgBuf << std::endl;
+		if (true == fileName.empty()) return;
+
+		Util::LogManager* logger = Util::LogManager::GetSingleton();
+		std::ofstream* fout = logger->GetFileStream();
+		fout->open(logger->GetDirectory() + fileName, std::ios_base::out | std::ios_base::app);
+		*fout << msgBuf << std::endl;
+		fout->close();
+
+		return;
+	}
+}
---
+++ BaseLib/Log.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef __BASELIB_LOG_H__
+#define __BASELIB_LOG_H__
+#include "Singleton.h"
+#include <fstream>
+#include <string>
+
+namespace Util {
+	class LogManager :public Singleton<LogManager> {
+	public:
+		void SetDirectory(const char* directory) { m_directory = directory; }
+		std::string GetDirectory() const { return m_directory; }
+		std::ofstream* GetFileStream() const { return m_writeFile; }
+
+	private:
+		DECLARE_SINGLETON(LogManager)
+		LogManager(const char* directory);
+		std::string m_directory;
+		std::ofstream* m_writeFile = nullptr;
+	};
+
+	/*
+	Logging(file name, message, ...)
+	If you do not write file name, it will be only displayed in console window.
+	*/
+	void Logging(std::string fileName, const char* msg, ...);
+}
+
+
+
+#endif //!__BASELIB_LOG_H__
