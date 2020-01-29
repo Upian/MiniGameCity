795ae35c629b16bd52fe23442a283d3f4f05ae4d
Jeonghwan-Yoo <dwgbjdhks2@gmail.com>
2020-01-29 16:11:45 +0900

    basePacket 재재수정

--- BaseLib/SocialPacket.cpp
+++ BaseLib/SocialPacket.cpp
@@ -1,20 +1,15 @@
 #include "SocialPacket.h"
 
 char* ChatAllRequest::Serialize() {
-	int basePacketTypeLength = sizeof(GetBasePacketType());
-	int socialPacketTypeLength = sizeof(GetSocialPacketType());
-	int chatAllPacketLength = sizeof(GetChatAllRequest()) + 1;
-	char* buf = new char[basePacketTypeLength + socialPacketTypeLength + chatAllPacketLength];
-	char* bufInit = buf;
-	TypeSerial(buf, GetBasePacketType());
-	TypeSerial(buf, GetSocialPacketType());
-	StringSerial(buf, GetChatAllRequest());
-	return bufInit;
+	TypeSerial(GetBasePacketType());
+	TypeSerial(GetSocialPacketType());
+	StringSerial(GetChatAllRequest());
+	return buf;
 }
 
 void ChatAllRequest::Deserialize(char* _buf) {
 	if (_buf == nullptr) return;
 
 	char* buf = _buf;
-	SetChatAllRequest(buf);
+	SetChatAllRequest(StringDeserial(buf));
 }
